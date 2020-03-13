2b68b7044d74e8a14e1a2264ee69c58a33390c05
Scott Lingner <slingner@gmail.com>
2020-03-13 09:28:04 -0700

    heroku deploy setup

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const app = require('./modules/app/app');
 const port = process.env.PORT || 8000;
+const cors = require('cors');
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
 
 app.listen(port, () => {
   console.log(`[petful-server] Listening on ${ port }.`);
 });
+
+
